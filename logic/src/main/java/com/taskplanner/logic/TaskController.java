460f0d1b4d42a4782e6a6667e89de264ac214c79
Nick <nick.hagens@hotmail.com>
2020-05-05 14:29:22 +0200

    Created method for finding all tasks

--- logic/src/main/java/com/taskplanner/logic/TaskController.java
+++ logic/src/main/java/com/taskplanner/logic/TaskController.java
@@ -1,11 +1,13 @@
 package com.taskplanner.logic;
 
+import lombok.var;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import javax.ws.rs.core.Response;
+import java.util.List;
 
 @RestController
 @RequestMapping("/task")
@@ -16,13 +18,24 @@ public class TaskController {
         this.repository = repository;
     }
 
-
     @CrossOrigin(origins = "http://localhost:3000")
     @PostMapping(path = "/new")
     public @ResponseBody Response newTask(@Valid @RequestBody Task task) {
         try {
             repository.save(task);
             return Response.ok().entity("Success").build();
+        }
+        catch (Exception ex) {
+            return Response.ok().entity("Fail").build();
+        }
+    }
+
+    @CrossOrigin(origins = "http://localhost:3000")
+    @GetMapping(path = "/findAll")
+    public @ResponseBody Response findAllTasks() {
+        try {
+            var tasks = repository.findAll();
+            return Response.ok().entity(tasks).build();
         }
         catch (Exception ex) {
             return Response.ok().entity("Fail").build();
