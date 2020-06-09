6806ceb96b8bdccb5618d2bd58a0dc470b9a5e0d
Nick <nick.hagens@hotmail.com>
2020-06-09 13:42:05 +0200

    Visual feedback when name is empty

--- selenium/src/test/java/org/taskplanner/selenium/taskplannerselenium/MainPageTest.java
+++ selenium/src/test/java/org/taskplanner/selenium/taskplannerselenium/MainPageTest.java
@@ -19,12 +19,12 @@ public class MainPageTest {
     }
 
     @Test
-    public void seleniumWorks() {
-        chromeDriver.get("https://google.com/");
-        WebElement element = chromeDriver.findElement(By.id("hplogo"));
+    public void siteRunning() {
+        chromeDriver.get(baseUrl);
+
+        WebElement element = chromeDriver.findElement(By.id("root"));
 
         Assert.assertNotNull(element);
-        Assert.assertEquals("https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png", element.getAttribute("src"));
 
         chromeDriver.close();
     }
@@ -42,7 +42,7 @@ public class MainPageTest {
 
         String currentUrl = chromeDriver.getCurrentUrl();
 
-        Assert.assertEquals("http://localhost:3000/index", currentUrl);
+        Assert.assertEquals(baseUrl + "index", currentUrl);
 
         chromeDriver.close();
     }
@@ -51,14 +51,14 @@ public class MainPageTest {
     public void taskNoEmptyName() {
         chromeDriver.get(baseUrl + "newTask");
 
-        chromeDriver.findElement(By.id("TaskName")).sendKeys("value", "");
+        //chromeDriver.findElement(By.id("TaskName")).sendKeys("value", "");
         chromeDriver.findElement(By.id("TaskDescription")).sendKeys("value", "Check2");
         chromeDriver.findElement(By.id("StartTime")).sendKeys("value", "2020-06-08T$16:00");
         chromeDriver.findElement(By.id("EndTime")).sendKeys("value", "2020-06-08T$17:00");
 
         chromeDriver.findElement(By.id("Save")).click();
 
-        WebElement alert = chromeDriver.findElement(By.id("invalidAlert"));
+        WebElement alert = chromeDriver.findElement(By.id("nameInvalid"));
         String currentUrl = chromeDriver.getCurrentUrl();
 
         Assert.assertNotNull(alert);
